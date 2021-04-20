5ec777d19dd03cac018ed4131bc9440a4a70b3e8
kobzkeny <46962737+kobzkeny@users.noreply.github.com>
2021-04-20 18:37:39 +0200

    Nowe testy w klasie MealTest

--- src/test/java/pl/devfoundry/testing/MealTest.java
+++ src/test/java/pl/devfoundry/testing/MealTest.java
@@ -16,7 +16,35 @@ class MealTest {
 
         //then
         assertEquals(28,discountedPrice);
+    }
+    @Test
+    void referencesToTheSameObjectShouldBeEqual(){
+        //given
+        Meal meal1 = new Meal(10);
+        Meal meal2 = meal1;
 
+        //then
+        assertSame(meal1,meal2);
+    }
 
+    @Test
+    void referencesToDiffrentObjectShouldNotBeEqual(){
+        //given
+        Meal meal1 = new Meal(10);
+        Meal meal2 = new Meal(15);
+
+        //then
+        assertNotSame(meal1,meal2);
+    }
+
+    @Test
+    void twoMealsShouldBeEqualWhenPriceAndNameIsTheSame(){
+        //given
+        //when
+        Meal meal1 = new Meal(35, "Burger");
+        Meal meal2 = new Meal(35,"Burger");
+
+        //then
+        assertEquals(meal1,meal2);
     }
 }
