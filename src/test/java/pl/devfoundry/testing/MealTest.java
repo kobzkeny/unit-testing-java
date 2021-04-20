6612307780cf42ed83251bf73fed2989b87876d6
kobzkeny <46962737+kobzkeny@users.noreply.github.com>
2021-04-20 18:19:40 +0200

    Dodanie klasy Meal i MealTest

---
+++ src/test/java/pl/devfoundry/testing/MealTest.java
@@ -0,0 +1,22 @@
+package pl.devfoundry.testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MealTest {
+
+    @Test
+    void shouldReturnDiscountedPrice() {
+        //given
+        Meal meal = new Meal(35);
+
+        //when
+        int discountedPrice = meal.getDiscountedPrice(7);
+
+        //then
+        assertEquals(28,discountedPrice);
+
+
+    }
+}
